26909c0b163b753320136d07213b4dad3d232163
aniketk9021 <aniketkhandagle304@gmail.com>
2026-01-15 15:40:22 +0530

    update

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.2.0
     </div>
-  </footer>aniket
+  </footer>
 
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
