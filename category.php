43a39557443d7fbd9d874e78d229055b63a4b681
aniketk9021 <aniketkhandagle304@gmail.com>
2026-01-17 09:38:23 +0530

    update

--- category.php
+++ category.php
@@ -1,18 +1,37 @@
 <?php
 include 'header.php';
-include './db/db.php';
+require_once __DIR__ . '/db/db.php';
+
+/* ------------------------
+   INSERT CATEGORY
+-------------------------*/
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo "hello";
-    echo $catgoryName = $_POST['category_name'];
-    echo $catgoryDescription = $_POST['category_description'];
 
-    $insertQuery = "INSERT INTO tbl_category(category_name,category_description)VALUES('$catgoryName','$catgoryDescription');";
+    $categoryName = trim($_POST['category_name']);
+    $categoryDescription = trim($_POST['category_description']);
 
-    $result = mysqli_query($conn, $insertQuery);
-    if ($result) {
+    if (!empty($categoryName)) {
+
+        $stmt = mysqli_prepare(
+            $conn,
+            "INSERT INTO tbl_category (category_name, category_description) VALUES (?, ?)"
+        );
+
+        mysqli_stmt_bind_param($stmt, "ss", $categoryName, $categoryDescription);
+        $result = mysqli_stmt_execute($stmt);
+
+        if ($result) {
+            header("Location: category.php");
+            exit;
+        } else {
+            echo "Insert Error: " . mysqli_error($conn);
+        }
+    } else {
+        echo "Category name is required";
     }
 }
 ?>
+
 <div class="content-wrapper">
     <div class="content-header">
         <div class="container-fluid">
@@ -22,69 +41,76 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 </div>
                 <div class="col-sm-6">
                     <button type="button" class="btn btn-success float-right" data-toggle="modal"
-                        data-target="#addCustomerModal">
-                        <i class="fas fa-user-plus"></i> Add New Category
+                        data-target="#addCategoryModal">
+                        <i class="fas fa-plus"></i> Add New Category
                     </button>
                 </div>
             </div>
         </div>
     </div>
-    <div class="modal fade" id="addCustomerModal" tabindex="-1" role="dialog">
-        <div class="modal-dialog modal-lg" role="document">
+
+    <!-- ADD CATEGORY MODAL -->
+    <div class="modal fade" id="addCategoryModal" tabindex="-1">
+        <div class="modal-dialog modal-lg">
             <div class="modal-content">
+
                 <form method="POST">
                     <div class="modal-header">
                         <h5 class="modal-title">Add New Category</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
+                        <button type="button" class="close" data-dismiss="modal">
+                            <span>&times;</span>
                         </button>
                     </div>
+
                     <div class="modal-body">
-                        <input type="hidden" name="action" value="add">
-
                         <div class="row">
+
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <label for="new_first_name">Category Name *</label>
-                                    <input type="text" class="form-control" id="new_first_name" name="category_name"
-                                        required>
+                                    <label>Category Name *</label>
+                                    <input type="text" class="form-control" name="category_name" required>
                                 </div>
                             </div>
+
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <label for="new_last_name">Category Description</label>
-                                    <input type="text" class="form-control" id="new_last_name"
-                                        name="category_description">
+                                    <label>Category Description</label>
+                                    <input type="text" class="form-control" name="category_description">
                                 </div>
                             </div>
+
                         </div>
-
-
                     </div>
+
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
                         <button type="submit" class="btn btn-success">Add Category</button>
                     </div>
                 </form>
+
             </div>
         </div>
     </div>
-    <table border="1" style=" margin-left : 30px; width: 800px; text-align: center;">
+
+    <!-- CATEGORY TABLE -->
+    <table border="1" style="margin-left:30px;width:800px;text-align:center;">
         <tr>
             <th>Category Name</th>
             <th>Category Description</th>
         </tr>
+
         <?php
         $selectQuery = "SELECT * FROM tbl_category";
         $result = mysqli_query($conn, $selectQuery);
+
         while ($row = mysqli_fetch_assoc($result)) {
             echo "<tr>";
-            echo "<td>" . $row['category_name'] . "</td>";
-            echo "<td>" . $row['category_description'] . "</td>";
+            echo "<td>{$row['category_name']}</td>";
+            echo "<td>{$row['category_description']}</td>";
             echo "</tr>";
         }
         ?>
     </table>
-
 </div>
-<?php include 'footer.php' ?>
+
+<?php include 'footer.php'; ?>
